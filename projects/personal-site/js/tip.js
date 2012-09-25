6778fc8b471708f329e544262c711af2b9ae1a64
shahide1 <shahide1@msu.edu>
2012-09-24 23:27:18 -0400

    personal site

---
+++ projects/personal-site/js/tip.js
@@ -0,0 +1,4 @@
+function tipofday() {
+	txt = "Always have a goal of how much you want to make with any given position.  Once you reach this goal, liquidate your position.  This will enable you to be more consistent with locking-in profits!"
+	alert(txt);
+}
